64ff042e5a4ef2c5b92809c85a8e85e43115f89b
sumitkumar785796 <sumitkumar785796@gmail.com>
2023-11-30 00:40:22 +0530

    update CODESSS

--- myapp/settings.py
+++ myapp/settings.py
@@ -163,9 +163,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Cloudinary configuration
 cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME",''),
+    api_key=os.environ.get("CLOUDINARY_API_KEY",''),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET",''),
 )
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
