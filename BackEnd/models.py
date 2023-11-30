f99f5a3b013a27ba3aea502518b13addf38a6e3b
sumitkumar785796 <sumitkumar785796@gmail.com>
2023-11-30 15:58:36 +0530

    add phone model

--- BackEnd/models.py
+++ BackEnd/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
+class PhoneNo(models.Model):
+    phone=models.CharField(max_length=15)
 class AddCate(models.Model):
     # img=CloudinaryField('img')
     img=models.URLField()
